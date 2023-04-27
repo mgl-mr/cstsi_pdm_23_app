62b6eb28fa26d92483bdf9a1043761347c8eb366
Miguel Rodrigues <miguelmoralesrodrigues371@gmail.com>
2023-04-27 11:24:36 -0300

    feat: filtrar lobby por jogo

--- src/screens/Lobbys/index.js
+++ src/screens/Lobbys/index.js
@@ -1,14 +1,24 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import {LobbyContext} from '../../context/LobbyProvider';
 import {JogosContext} from '../../context/JogoProvider';
 import Card from './Card';
 import AddFloatButton from '../../components/AddFloatButton';
-import {Container, Text, FlatList} from './styles';
+import JogosPicker from '../../components/JogosPicker';
+import {Container, FlatList, Button} from './styles';
+import {COLORS} from '../../assets/colors';
 
 const Lobbys = ({navigation}) => {
   const {lobbys} = useContext(LobbyContext);
   const {jogos} = useContext(JogosContext);
+  const [lobbysFiltrados, setLobbysFiltrados] = useState('');
+
+  useEffect(() => {
+    if (lobbysFiltrados == '') {
+      setLobbysFiltrados(lobbys);
+    }
+  }, [lobbysFiltrados]);
 
   const getJogo = idJogo => {
     const jogo = jogos.find(g => g.id === idJogo);
@@ -19,14 +29,23 @@ const Lobbys = ({navigation}) => {
     return <Card nome={item.nome} jogo={getJogo(item.idJogo)} />;
   };
 
+  const filtraLobbys = id => {
+    setLobbysFiltrados(lobbys.filter(l => l.idJogo == id));
+  }
+
   return (
     <Container>
-      <Text>LOBBYS</Text>
+      <JogosPicker jogos={jogos} onJogoSelecionado={filtraLobbys} />
       <FlatList
-        data={lobbys}
+        data={lobbysFiltrados}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
+      {lobbysFiltrados != lobbys && (
+        <Button onPress={() => setLobbysFiltrados(lobbys)}>
+          <Icon name="close-circle-outline" size={30} color={COLORS.primary} />
+        </Button>
+      )}
       <AddFloatButton onClick={() => navigation.navigate('AdicionarLobby')} />
     </Container>
   );
--- src/screens/Lobbys/styles.js
+++ src/screens/Lobbys/styles.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components/native';
+import {COLORS} from '../../assets/colors';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   align-items: center;
 `;
 
-export const Text = styled.Text`
-  font-size: 24px;
+export const Button = styled.TouchableOpacity`
+  border-width: 0px;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 10px;
+  right: 10px;
 `;
 
 export const FlatList = styled.FlatList`
